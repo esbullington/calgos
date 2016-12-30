b35779673a2de0559beb2ef098718d0afe6c4356
Eric S. Bullington <eric.s.bullington@gmail.com>
2016-12-30 13:59:05 -0500

    Standardize function names

--- mergesort.c
+++ mergesort.c
@@ -4,39 +4,39 @@
 #include "utils.h"
 
 
-void merge(int *array, int *first_half, int left_count,
-           int *second_half,int right_count)
+void merge(int *array, int *left, int left_count,
+           int *right,int right_count)
 {
-	// i: marks the index of first subarray (first_half)
-	// j: marks the index of second subarray (second_half)
+	// i: marks the index of first subarray (left)
+	// j: marks the index of second subarray (right)
 	// k: marks the index of merged subarray (array)
 	int i, j, k;
 	i = 0; j = 0; k =0;
 
 	while(i < left_count && j < right_count) {
-		if(first_half[i]  < second_half[j]) {
-            array[k] = first_half[i];
+		if(left[i]  < right[j]) {
+            array[k] = left[i];
             k++; i++;
         } else{
-            array[k] = second_half[j];
+            array[k] = right[j];
             k++; j++;
         }
 	}
 
     // Append subarrays back onto main array
 	while(i < left_count) {
-        array[k] = first_half[i];
+        array[k] = left[i];
         k++; i++;
     }
 	while(j < right_count) {
-        array[k] = second_half[j];
+        array[k] = right[j];
         k++; j++;
     }
 }
 
 void mergesort(int *array, size_t count)
 {
-	int mid, *first_half, *second_half;
+	int mid, *left, *right;
     // base condition. If the array count < 2, stop recursing
 	if (count < 2) return;
 
@@ -49,18 +49,18 @@ void mergesort(int *array, size_t count)
         perror("Mergesort failed");
         exit(EXIT_FAILURE);
     }
-	first_half = a->first_half;
-	second_half = a->second_half;
+	left = a->first_half;
+	right = a->second_half;
 
-    // recursively partition the array to the left or "first_half"
-	mergesort(first_half, mid);
-    // recursively partition the array to the right or "second_half"
-	mergesort(second_half, count - mid);
-    // then, recursively merge first_half and second_half
+    // recursively partition the array to the left or "left"
+	mergesort(left, mid);
+    // recursively partition the array to the right or "right"
+	mergesort(right, count - mid);
+    // then, recursively merge left and right
     // back into the array as sorted list.
-	merge(array, first_half, mid, second_half, count - mid); 
+	merge(array, left, mid, right, count - mid); 
 
     free(a);
-	free(first_half);
-	free(second_half);
+	free(left);
+	free(right);
 }
